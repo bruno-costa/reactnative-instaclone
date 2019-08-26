8e2c8eb97c1a965ed17f7daf05657e45e4254a32
Bruno Avelino <brunoac.cc@gmail.com>
2019-08-25 22:58:57 -0300

    feed screen: estrutura basica criada

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,25 @@
+import React from 'react';
+import {Image} from 'react-native';
 import {createAppContainer, createStackNavigator} from 'react-navigation';
+
+import logo from './assets/instagram.png';
 
 import Feed from './screens/Feed';
 
 const Routes = createAppContainer(
-  createStackNavigator({
-    Feed,
-  }),
+  createStackNavigator(
+    {
+      Feed,
+    },
+    {
+      defaultNavigationOptions: {
+        headerTitle: <Image source={logo} />,
+        headerStyle: {
+          backgroundColor: '#f5f5f5',
+        },
+      },
+    },
+  ),
 );
 
 export default Routes;
