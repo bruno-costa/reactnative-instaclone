6a2d8ab0da084096a54fdb71dfc02aaa4fdddece
Bruno Avelino <brunoac.cc@gmail.com>
2019-08-25 23:41:14 -0300

    pulltorefresh ok

--- src/screens/Feed/index.js
+++ src/screens/Feed/index.js
@@ -1,23 +1,54 @@
 import React, {useState, useEffect} from 'react';
 import {View, FlatList} from 'react-native';
 
-import {Post, Header, Avatar, Name, PostImage, Description} from './styles';
+import {
+  Post,
+  Header,
+  Avatar,
+  Name,
+  PostImage,
+  Description,
+  Loading,
+} from './styles';
+
+const PER_PAGE = 5;
 
 export default function Feed() {
   const [feed, setFeed] = useState([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(Infinity);
+  const [loading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
 
-  useEffect(() => {
-    async function loadFeed() {
-      const response = await fetch(
-        'http://localhost:3000/feed?_expand=author&_limit=5&_page=1',
-      );
-
-      const data = await response.json();
-
-      setFeed(data);
+  async function loadPage(pageNumber = page, shoudRefresh = false) {
+    if (total <= page * PER_PAGE) {
+      return;
     }
 
-    loadFeed();
+    setLoading(true);
+
+    const response = await fetch(
+      `http://localhost:3000/feed?_expand=author&_limit=${PER_PAGE}&_page=${pageNumber}`,
+    );
+
+    const data = await response.json();
+    const totalItems = response.headers.get('X-Total-Count');
+
+    setTotal(totalItems * 1);
+    setFeed(shoudRefresh ? data : [...feed, ...data]);
+    setPage(pageNumber + 1);
+    setLoading(false);
+  }
+
+  async function refreshList() {
+    setRefreshing(true);
+    await loadPage(1, true);
+    setRefreshing(false);
+  }
+
+  useEffect(() => {
+    loadPage();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
@@ -25,13 +56,18 @@ export default function Feed() {
       <FlatList
         data={feed}
         keyExtractor={post => String(post.id)}
+        onEndReachedThreshold={0.1}
+        onEndReached={() => loadPage()}
+        onRefresh={refreshList}
+        refreshing={refreshing}
+        ListFooterComponent={loading && <Loading />}
         renderItem={({item}) => (
           <Post>
             <Header>
               <Avatar source={{uri: item.author.avatar}} />
               <Name>{item.author.name}</Name>
             </Header>
-            <PostImage source={{uri: item.image}} />
+            <PostImage ratio={item.aspectRatio} source={{uri: item.image}} />
             <Description>
               <Name>{item.author.name}</Name>
               {item.description}
